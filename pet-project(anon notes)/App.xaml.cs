b0dc33d8c581926b56736ec0bbe4116f305499db
dinoden <dinozer1234@gmail.com>
2024-10-20 03:35:12 +0300

    Add project files.

---
+++ pet-project(anon notes)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pet_project_anon_notes_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ pet-project(anon notes)/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using anonotes.Core;
+
+namespace pet_project_anon_notes_.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
