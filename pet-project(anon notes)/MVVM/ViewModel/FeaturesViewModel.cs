0a0fbbdd98433ac6aa496235f08e1ea3b608a59b
dinoden <dinozer1234@gmail.com>
2024-10-22 15:18:43 +0300

    git add .

---
+++ pet-project(anon notes)/MVVM/ViewModel/FeaturesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pet_project_anon_notes_.MVVM.ViewModel
+{
+    internal class FeaturesViewModel
+    {
+    }
+}
--- pet-project(anon notes)/MVVM/ViewModel/MainViewModel.cs
+++ pet-project(anon notes)/MVVM/ViewModel/MainViewModel.cs
@@ -6,9 +6,31 @@ namespace pet_project_anon_notes_.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand FeaturesViewCommand { get; set; }
+
+        public FeaturesViewModel FeaturesVM { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
+            HomeVM=new HomeViewModel();
+            FeaturesVM=new FeaturesViewModel();
+            CurrentView = HomeVM;
 
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            FeaturesViewCommand = new RelayCommand(o => { CurrentView = FeaturesVM; });
         }
     }
 }
